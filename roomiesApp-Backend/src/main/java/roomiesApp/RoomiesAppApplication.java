fb908e0ff1d26cffcc46fe3eddee5539e592b95c
ben12mwaniki <ben12mwaniki@gmail.com>
2021-06-15 09:48:16 -0400

    added global wrapper

---
+++ roomiesApp-Backend/src/main/java/roomiesApp/RoomiesAppApplication.java
@@ -0,0 +1,26 @@
+package roomiesApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+
+@RestController
+@SpringBootApplication
+public class RoomiesAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoomiesAppApplication.class, args);
+	}
+	
+	
+	 @RequestMapping("/")
+	  public String greeting(){
+	    return "Hello world!";
+	  }
+	
+	
+
+}
---
+++ roomiesApp-Backend/src/test/java/roomiesApp/RoomiesAppApplicationTests.java
@@ -0,0 +1,15 @@
+package roomiesApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoomiesAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+		
+		
+	}
+
+}
